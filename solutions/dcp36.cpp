d7ff3061bc850b4ac8bc7d3fa6b991b3db2d7ffe
prakash-aditya <adityaprakash633@gmail.com>
2019-03-23 16:55:32 +0530

    Added daily coding problem #36

---
+++ solutions/dcp36.cpp
@@ -0,0 +1,38 @@
+/*
+This problem was asked by Dropbox.
+
+Given the root to a binary search tree, 
+find the second largest node in the tree.
+*/
+
+//only the function is defined.
+void kthLargest(Node *root, int k)
+{
+    //Your code here
+    Node *res = NULL;
+    
+
+    //go to the largest node to the rightmost node, 
+    //keeping its parent as second largest.
+    while(root->right){
+        res = root;
+        root = root->right;
+    }
+    
+    //if the rightmost node(largest) has any left node,
+    // then it is greater than its parent, so becomes second largest.
+    if(root->left){
+        res = root->left;
+        root= root->left;
+    }
+    
+    //now if this left node has right node then its rightmost node is largest of all,
+    //and is hence our second largest node.
+    while(root->right){
+        root = root->right;
+        res = root;
+    }
+    
+    return res;
+    
+}
