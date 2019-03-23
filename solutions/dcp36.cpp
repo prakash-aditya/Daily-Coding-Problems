7ca71d07c0a80b46786a075412556be48da1dc80
prakash-aditya <adityaprakash633@gmail.com>
2019-03-23 16:59:18 +0530

    Added daily coding problem #36

--- solutions/dcp36.cpp
+++ solutions/dcp36.cpp
@@ -6,25 +6,22 @@ find the second largest node in the tree.
 */
 
 //only the function is defined.
-void kthLargest(Node *root)
+void secondLargest(Node *root)
 {
     Node *res = NULL;
     
-
     //go to the largest node to the rightmost node, 
     //keeping its parent as second largest.
     while(root->right){
         res = root;
         root = root->right;
     }
-    
     //if the rightmost node(largest) has any left node,
     // then it is greater than its parent, so becomes second largest.
     if(root->left){
         res = root->left;
         root= root->left;
     }
-    
     //now if this left node has right node then its rightmost node is largest of all,
     //and is hence our second largest node.
     while(root->right){
